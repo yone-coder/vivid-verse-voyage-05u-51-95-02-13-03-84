aee84611f6cd067f3ef31ca7652aa6890560e265
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-07-11 12:01:04 -0400

    Update StepTwoTransfer.tsx

--- src/components/transfer/StepTwoTransfer.tsx
+++ src/components/transfer/StepTwoTransfer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { AlertCircle } from 'lucide-react';
+import { AlertCircle, PhoneInput} from 'lucide-react';
 
 interface ReceiverDetails {
   firstName: string;
